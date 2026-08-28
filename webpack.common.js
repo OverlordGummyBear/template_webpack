94e286dd2b9c61526ad553749767e81cd25f2bef
OverlordGummyBear <197521801+OverlordGummyBear@users.noreply.github.com>
2026-08-28 13:29:49 +0200

    Complete webpack template with README file

--- webpack.common.js
+++ webpack.common.js
@@ -9,16 +9,11 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 //Webpack server command "npx webpack serve"
 
 export default {
-  mode: "development",
   entry: "./src/index.js",
   output: { //output from running webpack
     filename: "main.js",
     path: path.resolve(import.meta.dirname, "dist"),
     clean: true, //clean the dist directory every time we create a new output
-  },
-  devtool: "eval-source-map", //any error messages reference files and lines from development code
-  devServer: {
-    watchFiles: ["./src/index.html"], //tell Webpack to watch for changes in the html file as html files are not automatically included in being watched for changes
   },
   plugins: [
     new HtmlWebpackPlugin({ //enable us to include html files in webpack
@@ -31,7 +26,7 @@ export default {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"], //the loader order matters. Chain is executed right to left
       },
-      { //enable us to use image files we reference in our html template  e.g. as src of an <img>
+      { //enable us to include html files and process them with html-loader (needed for html-webpack-plugin's template)
         test: /\.html$/i,
         use: ["html-loader"],
       },
